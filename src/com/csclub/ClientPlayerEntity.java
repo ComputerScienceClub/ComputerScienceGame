693d74f5bc3d93d4c41ea3b66f85c456c55111f7
Andrew Yates <>
2014-10-20 12:45:03 -0700

    Implement Entity in ClientPlayerEntity

--- src/com/csclub/ClientPlayerEntity.java
+++ src/com/csclub/ClientPlayerEntity.java
@@ -3,7 +3,45 @@ package com.csclub;
 /**
  * This is the player controlled by THIS CLIENT.
  */
-public class ClientPlayerEntity
+public class ClientPlayerEntity implements Entity
 {
+    private double x;
+    private double y;
+
+    public ClientPlayerEntity()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public ClientPlayerEntity(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+    @Override
+    public double getX()
+    {
+        return x;
+    }
+
+    @Override
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+
+    @Override
+    public double getY()
+    {
+        return y;
+    }
     
 }
